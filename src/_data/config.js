f135e83afe21e4971ddc883a628a527470dcfc66
pankaj-tagglabs <77740673+pankaj-tagglabs@users.noreply.github.com>
2023-05-06 21:37:38 +0530

    Update config.js

--- src/_data/config.js
+++ src/_data/config.js
@@ -16,8 +16,8 @@ module.exports = {
 
   // Profile picture on home page. Set to null to leave out.
   profilePic: {
-    best: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/Ankur.jpg",
-    small: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/Ankur.jpg",
+    best: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/AnkurTaneja.jpg",
+    small: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/AnkurTaneja.jpg",
   },
 
   // Name pronunciation. Set to null to leave out.
@@ -31,13 +31,13 @@ module.exports = {
   // that sites like FB change their preview. Set to null to leave out.
   openGraph: {
     absolute: true,
-    url: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/Ankur.jpg",
+    url: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/AnkurTaneja.jpg",
   },
 
   // Path to Twitter preview image. Set to null to leave out.
   twitterPreview: {
     absolute: true,
-    url: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/Ankur.jpg",
+    url: "https://d35hr0os3yc7ki.cloudfront.net/AnkurTaneja/AnkurTaneja.jpg",
   },
 
   // Links to your other websites.
